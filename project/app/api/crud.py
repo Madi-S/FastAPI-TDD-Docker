32ab1e3020822b2a17545dbf74c5a47dba6deb41
Madi-S <shaiken.m@mail.ru>
2022-12-15 16:25:13 +0600

    Tests enhanced

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -10,7 +10,7 @@ async def post(payload: SummaryPayloadSchema) -> int:
 
 
 async def get(id: int) -> dict | None:
-    """Returns summary by id"""
+    """Returns summary by id or `None` if not found"""
     summary = await TextSummary.filter(id=id).first().values()
     if summary:
         return summary
@@ -19,5 +19,21 @@ async def get(id: int) -> dict | None:
 
 
 async def get_all() -> list:
+    """Returns all summaries"""
     summaries = await TextSummary.all().values()
     return summaries
+
+
+async def delete(id: int) -> int:
+    """Returns deleted summary id"""
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryPayloadSchema) -> dict | None:
+    """Returns updated summary or `None` if not found"""
+    summary = await TextSummary.filter(id=id).update(url=payload.url, summary=payload.summary)
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary
+    return None
